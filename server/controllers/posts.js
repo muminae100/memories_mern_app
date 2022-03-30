38a8d46c69143957441afeb36dbc9a14c5a23ab9
foreverkenyan <smuminaetx100@gmail.com>
2022-03-30 16:44:34 +0300

    Implement get all posts route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
+
 export const getPosts = async (req, res) => { 
-    res.send('Hello world!');
+    try {
+        const postMessages = await PostMessage.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => { 
+    res.send('Create post!');
 }
 
 
