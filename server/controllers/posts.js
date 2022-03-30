ec20a9d5e8c3ed08c763cbf28e7cecd390f8e8d0
foreverkenyan <smuminaetx100@gmail.com>
2022-03-30 17:53:49 +0300

    Add home UI interface

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -16,9 +16,18 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = async (req, res) => { 
-    res.send('Create post!');
-}
+export const createPost = async (req, res) => {
+    const { title, message, selectedFile, creator, tags } = req.body;
 
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
 
 export default router;
