6e9a5a54169b15a522819e63380d067f8566ac6b
foreverkenyan <smuminaetx100@gmail.com>
2022-03-31 16:57:32 +0300

    Add Procfile deploy backend to heroku

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.get('/', (req,res) => {
+  res.send('Hello World! This a Memories app API');
+});
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
