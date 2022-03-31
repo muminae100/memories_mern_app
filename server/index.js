6144669aa70ca26c814fa7841f2fe306938bd218
foreverkenyan <smuminaetx100@gmail.com>
2022-03-31 16:37:06 +0300

    Add environment variables

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -14,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://simon:muminaetx100@cluster0.mrdz6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT|| 5000;
 
